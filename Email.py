52d629e7b393c8d0a9f3138b1a62e331b44840a0
AliceCullen-html <marcusvcunha0800@hotmail.com>
2022-08-09 00:02:30 -0300

    Enviando Email Att

--- Email.py
+++ Email.py
@@ -6,7 +6,7 @@
 # Primeiro import o win32com.client
 import win32com.client
 
-outlook = win32com.cient.Dispatch("Outlook.Application")
+outlook = win32com.client.Dispatch("Outlook.Application")
 conta = ''
 
 
@@ -22,19 +22,22 @@ mail = outlook.CreateItem(0)
 
 if conta:
     mail._oleobj_.Invoke(*(64209, 0, 8, 0, conta))
-l
 
 
-mail.To = 'testanto@hotmail.com'
+mail.To = 'test@hotmail.com'
 mail.Subject = 'Email vindo do Outlook'
-mail.CC = 'email@gmail.com'
-mail.BCC = 'email@gmail.com'
+#mail.CC = 'email@gmail.com'
+#mail.BCC = 'email@gmail.com'
+
+# se preferir pode mandar um email mais simples usando o mail.Body
+
+
 mail.HTMLBody = '<h1> Corpo do Email em HTML </h1>'
 '<p> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce lacinia, enim ut egestas pulvinar, lorem nisi consectetur leo, eget euismod ante leo ut dui. Nunc at tempor erat, a imperdiet nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc ex nisl, congue a ante sit amet, sagittis euismod purus. Morbi quam ligula, iaculis sed tincidunt eget, vehicula eget erat. Proin neque felis, aliquet sed odio id, eleifend efficitur velit. Nulla id lacus dictum neque bibendum ullamcorper ut eu tortor. Donec mollis risus nunc, eu scelerisque enim suscipit a. </p>'
 
 
 # Anexos (pode colocar quantos quiser):
-attachment = r'C:\Users\vulco\Downloads\test.pdf'
+attachment = r'C:\Users\vulco\Downloads\Cvnew1.pdf'
 mail.Attachments.Add(attachment)
 
 
